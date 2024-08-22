1ec59ae9ca3a54d3abdb59f9aa803ecb1db7dee9
codewithkrishan2 <codewithkrishan@gmail.com>
2024-08-22 13:49:56 +0530

    minor-changes

--- src/main/java/com/bank/controllers/BankRestController.java
+++ src/main/java/com/bank/controllers/BankRestController.java
@@ -3,6 +3,7 @@ package com.bank.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,49 +23,41 @@ public class BankRestController {
 	private BankService bankService;
 	private BranchService branchService;
 	private ServiceOfBankService servicesOfBankService;
-	
-	BankRestController(BankService bankService, BranchService branchService, ServiceOfBankService servicesOfBankService) {
+
+	BankRestController(BankService bankService, BranchService branchService,
+			ServiceOfBankService servicesOfBankService) {
 		super();
 		this.bankService = bankService;
 		this.branchService = branchService;
 		this.servicesOfBankService = servicesOfBankService;
 	}
-	
-	@GetMapping("/all-banks")
-	public List<Bank> getAllBanks() {
-		return bankService.getAllBanks();
+
+	@GetMapping("/list")
+	public ResponseEntity<List<Bank>> listBanks(Model model) {
+		return ResponseEntity.ok(bankService.getAllBanks());
 	}
-	
-	@GetMapping("/all-branches")
-	public List<Branch> getAllBranches() {
-		return branchService.getBranchesByBankName("SBI");
-	}
-	
-	@GetMapping("/all-services")
-	public List<Services> getAllServices() {
-		return servicesOfBankService.getServicesByBankName("SBI");
-	}
-	
+
 	@GetMapping("/branches/{bankName}")
-    public ResponseEntity<List<Branch>> getBranches(@PathVariable String bankName) {
-		Bank bank = bankService.getBankByName(bankName);
-        if (bank != null) {
-        	List<Branch> branches = branchService.getBranchesByBankName(bankName);
-            return ResponseEntity.ok(branches);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-	
+	public ResponseEntity<?> getBranches(@PathVariable String bankName) {
+		try {
+			Bank bank = bankService.getBankByName(bankName);
+			List<Branch> branches = branchService.getBranchesByBankName(bankName);
+			return ResponseEntity.ok(branches);
+		} catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
 	@GetMapping("/services/{bankName}")
-    public ResponseEntity<List<Services>> getServices(@PathVariable String bankName) {
-		Bank bank = bankService.getBankByName(bankName);
-        if (bank != null) {
-            List<Services> services = servicesOfBankService.getServicesByBankName(bankName);
-            return ResponseEntity.ok(services);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-	
+	public ResponseEntity<List<Services>> getServices(@PathVariable String bankName) {
+		try {
+			Bank bank = bankService.getBankByName(bankName);
+			List<Services> services = servicesOfBankService.getServicesByBankName(bankName);
+			return ResponseEntity.ok(services);
+		} catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
+
+	}
+
 }
